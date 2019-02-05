8d8acc341aac199bbec77a9cd15251fd56ebe035
Prudence Codjia <pcodjia@ironguard.net>
2019-02-04 21:09:25 -0500

    Adding apps.js file
    Updating

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
-$(document).ready(function){
-    //Let instantiate our Firebase database
+$(document).ready(function(){
+    //Initialize Firebase
     var config = {
         apiKey: "AIzaSyD2qxtt3hT8keatj9rBNzt11vxRpFJgtjA",
         authDomain: "train-scheduler-a3cef.firebaseapp.com",
@@ -10,5 +10,27 @@ $(document).ready(function){
     };
     firebase.initializeApp(config);
 
-    
-}
+    var dataRef = firebase.database();
+    // Creating a Jquery function with .on argument and another callback func.
+    $("#add-train-btn").on("click", function (event) {
+        event.preventDefault();
+
+ // grabs user inputs
+
+        var name = $("#train-name-input").val().trim();
+        var destination = $("#dest-input").val().trim();
+        var train = $("#firstTrain-input").val().trim();
+        var frequency = $("#freq-input").val().trim();
+
+        // Code for the push
+        dataRef.ref().push({
+
+            name: name,
+            dest: destination,
+            train: train,
+            frequency: frequency,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+
+
+});
