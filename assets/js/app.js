fbe146d437f7d1f3c355016cf5751a1c96305d5f
Prudence Codjia <pcodjia@ironguard.net>
2019-02-04 20:35:56 -0500

    Adding apps.js
    Making some logics

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function){
+    //Let instantiate our Firebase database
+    var config = {
+        apiKey: "AIzaSyD2qxtt3hT8keatj9rBNzt11vxRpFJgtjA",
+        authDomain: "train-scheduler-a3cef.firebaseapp.com",
+        databaseURL: "https://train-scheduler-a3cef.firebaseio.com",
+        projectId: "train-scheduler-a3cef",
+        storageBucket: "train-scheduler-a3cef.appspot.com",
+        messagingSenderId: "765696189083"
+    };
+    firebase.initializeApp(config);
+
+    
+}
